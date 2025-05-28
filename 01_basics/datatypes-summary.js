0d8e16281e224810eee44855d1c125e493ab550c
Gangas09 <gangasnanadhal2005@gmail.com>
2025-05-28 06:46:29 +0000

    Datatypes smmary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,34 @@
+ // # Primitive
+
+ // 7 types : String , Number , Boolean , Null , Undefined , BigInt 
+
+const score = 100
+const scoreValue = 100.3
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+console.log(id == anotherId);
+
+// const bigNumber = 124325865486n  // BigInt
+
+
+ // Reference (Non primitive)
+
+ // Array , Objects , Functions
+
+
+
+ const heros = ["Shaktiman", "Naagraj", " doga"]
+ let myObj =
+ {
+    name: "Sipu",
+    age: 22,
+ }
+
+ const myfunction = function(){
+console.log("Hello world");
+} 
+console.log(typeof myfunction);
